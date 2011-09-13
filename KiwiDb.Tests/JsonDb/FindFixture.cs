cd9971aa497725191333d68fdfc68f810cd68164
Joakim Larsson <mimmertim@gmail.com>
2011-09-13 19:31:32 +0200

    FindFixture.cs() bug - if no indices were specifiead at all, all records were returned

--- KiwiDb.Tests/JsonDb/FindFixture.cs
+++ KiwiDb.Tests/JsonDb/FindFixture.cs
@@ -39,5 +39,39 @@ namespace KiwiDb.Tests.JsonDb
 
             CollectionAssert.AreEqual(new[] {"0", "10", "20"}, objectsWithTag1);
         }
+
+        [Test]
+        public void NoIndex()
+        {
+            var coll = GetCollection();
+
+            Func<int, int[]> getTags = i =>
+            {
+                if ((i == 0) || (i == 10) || (i == 20))
+                {
+                    return new[] { 1, 2, 3 };
+                }
+                return null;
+            };
+
+            var objects = (from i in Enumerable.Range(0, 100)
+                           select new
+                           {
+                               Key = i.ToString(),
+                               Value = i,
+                               Tags = getTags(i)
+                           }).ToArray();
+
+            foreach (var o in objects)
+            {
+                coll.Update(o.Key, o);
+            }
+
+            //coll.Indices.EnsureIndex("Tags");
+            var objectsWithTag1 = coll.Find(new { Tags = 1 }).Select(kv => kv.Key).OrderBy(s => s).ToArray();
+
+            CollectionAssert.AreEqual(new[] { "0", "10", "20" }, objectsWithTag1);
+            
+        }
     }
 }
