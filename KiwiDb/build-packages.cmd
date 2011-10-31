a9f61abe17d09dbd82b67621a9709db3a54250bd
Joakim Larsson <mimmertim@gmail.com>
2011-10-31 22:03:00 +0100

    Repaierd more accidents

--- KiwiDb/build-packages.cmd
+++
@@ -1,3 +0,0 @@
-rem set PATH=%PATH%;..\.nuget
-rem PATH
-..\.nuget\nuget.exe pack .\KiwiDb.csproj -build -prop Configuration=Release;Platform=AnyCPU;SolutionDir=${ProjectDir}\.. -OutputDirectory . -exclude license.txt
